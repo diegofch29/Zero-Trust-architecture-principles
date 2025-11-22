166e3565fc75732926b444bdc598927e8ee662d2
diego chinchilla <diegofch29@hotmail.com>
2025-11-22 09:48:38 -0500

    Remove OpenAPI for .NET 8.0 compatibility

--- backend/Program.cs
+++ backend/Program.cs
@@ -48,17 +48,9 @@ builder.Services.AddCors(options =>
         });
 });
 
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
 app.UseCors("AllowReactApp");
 
 app.UseHttpsRedirection();
